c88bc43ee37a320d6c07acedb3cb4e307c04df7a
Dmitriy Kharlashin <kharlashin.dv@gmail.com>
2018-04-29 13:46:49 +0300

    Add like btn and counter

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,50 +1,61 @@
 import React from "react";
 
 export default class MovieItem extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isShowOverview: false
-    };
-  }
+        this.state = {
+            likeCount: 0,
+            isShowOverview: false
+        };
+    }
 
-  render() {
-    const { item } = this.props;
-    // console.log("state of MovieItem", this.state);
-    // console.log("props", this.props);
-    console.log(this.props);
-    return (
-      <div className="card" style={{ width: "100%" }}>
-        <img
-          className="card-img-top"
-          src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
-          alt=""
-        />
-        <div className="card-body">
-          <h6 className="card-title">{item.title}</h6>
-          <p className="mb-0">Rating: {item.vote_average}</p>
-          <button
-            onClick={() => {
-              this.setState({
-                isShowOverview: true
-              });
-            }}
-          >
-            Show overview
+    render() {
+        const { item } = this.props;
+        // console.log("state of MovieItem", this.state);
+        // console.log("props", this.props);
+        console.log(this.props);
+        return (
+            <div className="card" style={{ width: "100%" }}>
+                <img
+                    className="card-img-top"
+                    src={`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}
+                    alt=""
+                />
+                <div className="card-body">
+                    <h6 className="card-title">{item.title}</h6>
+                    <p>Rating: {item.vote_average}</p>
+                    <p>Likes: {this.state.likeCount}</p>
+                    <button
+                        onClick={() => {
+                            this.setState({
+                                likeCount: ++this.state.likeCount
+                            });
+                        }}
+                    >
+                        Like
+        </button>
+                    <button
+                        onClick={() => {
+                            this.setState({
+                                isShowOverview: true
+                            });
+                        }}
+                    >
+                        Show overview
           </button>
-          <button
-            onClick={() => {
-              this.setState({
-                isShowOverview: false
-              });
-            }}
-          >
-            Hide overview
+                    <button
+                        onClick={() => {
+                            this.setState({
+                                isShowOverview: false
+                            });
+                        }}
+                    >
+                        Hide overview
           </button>
-          {this.state.isShowOverview ? <p>{item.overview}</p> : null}
-        </div>
-      </div>
-    );
-  }
+                    {this.state.isShowOverview ? <p>{item.overview}</p> : null}
+                </div>
+            </div>
+        );
+    }
 }
