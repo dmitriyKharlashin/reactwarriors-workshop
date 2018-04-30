a95c1de419113aa6b902c28b3d86406a81c80f95
Dmitriy Kharlashin <kharlashin.dv@gmail.com>
2018-04-30 08:54:18 +0300

    Style fixes

--- src/components/Movies/Navigation.jsx
+++ src/components/Movies/Navigation.jsx
@@ -61,11 +61,11 @@ class Navigation extends Component {
                   }.jpg?s=64"`}
                 alt=""
               />
-              <a className="dropdown-toggle d-flex nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="dropdown-toggle d-flex nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ cursor: "pointer" }}>
                 <h6 className="mb-0">{user.username}</h6>
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" onClick={resetUser}>Logout</a>
+                <a className="dropdown-item" style={{ cursor: "pointer" }} onClick={resetUser}>Logout</a>
               </div>
             </div>
           </div>
